9127e2211b30639e2abd06fd7184083706829be5
matrixcollege1980 <88628396+matrixcollege1980@users.noreply.github.com>
2022-02-03 20:02:55 -0500

    Create youtubeSearch.py

---
+++ youtubeSearch.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.get("https://www.youtube.com/")
+
+
+def youtube_search():
+    search_youtube = driver.find_element(By.NAME, 'search_query')
+    search_youtube.send_keys("Tutorial of Github")
