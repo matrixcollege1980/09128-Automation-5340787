40130b95ba56bdb538af6325c5f30c7a3f89cea0
matrixcollege1980 <88628396+matrixcollege1980@users.noreply.github.com>
2022-02-03 22:48:34 -0500

    Update Google_result.py

--- Google_result.py
+++ Google_result.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-
+import pytest
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
 driver.get("https://www.google.com/")
 
-
 def google_search():
-    search_google = driver.find_element(By.NAME, 'q')
+    search_google = driver.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[1]/div/span/svg')
     search_google.send_keys("Github Tutorial")
+    search_google.click()
